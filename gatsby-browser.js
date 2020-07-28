c727c8085a5de4860a57898d8a859b5ece0a62ed
olavurellefsen <oe@tokni.com>
2020-07-28 10:39:11 +0100

    Add support for IE and old Safari

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
 import "./src/styles/global.css"
+export const onClientEntry = async () => {
+    // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+    if (!(`IntersectionObserver` in window)) {
+      await import(`intersection-observer`)
+      console.log(`# IntersectionObserver is polyfilled!`)
+    }
+    return true
+  }
